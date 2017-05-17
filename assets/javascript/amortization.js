4f28a152242452d1b41ffb58f322e99e1fa8e991
Brian Hendrickson <brian.hendrickson@live.com>
2017-05-16 22:24:37 -0700

    updates regex

--- assets/javascript/amortization.js
+++ assets/javascript/amortization.js
@@ -1,29 +1,42 @@
 $(document).ready(function() {
 	$("body").addClass("bg-primary");
 	$("input").addClass("text-muted");
-	$("form").on("submit", function(e) {
-		e.preventDefault();
-
-		var interest = $('form input[name="rate');
-		var validateInterest = /^\d+(\.\d{0,2})?/g;
-		if (interest.val() == '' || !validateInterest.test(interest.val()))
+	$("form").on("submit", function(e) {		// Run this function when submitting form button
+		e.preventDefault();		// Prevent default behavior when clicking submit button on form.
+													// Run this function instead.
+		var interest = $('form input[name="rate');	// variable records rate from text field as object
+		var validateInterest = /^(\d{0,2}\.{0,1}\d{0,2})$/g; // regex to capture valid interest rates 00.00%
+		if (interest.val() == '' || !validateInterest.test(interest.val())) // if value of interest rate is blank or doesn't follow regex rule
 		{
 			alert('Please enter a valid interest rate percentage');
-			return false;
+			return false;		// exit function due to invalid interest rate
 		}
 
-		calc();
-	});
-
-	$(':input#rate').change(function() {
-		var interest = $('form input[name="rate');
-		var validateInterest = /^\d{1}$/g;
-		console.log(interest.val() == validateInterest.test(interest.val()));
-		if (interest.val() == validateInterest.test(interest.val())) {
+		if (validateInterest.test(interest.val())) {
 			$(this).val(function(index, old) { return old.replace(/[^0-9\.]/g, '') + '.00%'; });
 		} else {
-    		$(this).val(function(index, old) { return old.replace(/[^0-9\.]/g, '') + '%'; });
-  		}
+			$(this).val(function(index, old) { return old.replace(/[^0-9\.]/g, '') + '%'; });
+		}
+
+		calc();		// Calculator amortized loan information
+	});
+
+	$(':input#rate').change(function() {		// jquery run function when input rate textbox changes value
+		var interest = $('form input[name="rate');
+		console.log("interest: " + interest.val());
+		var validateInterest = /^\d{1}%$/g;
+		console.log(validateInterest.test(interest.val()));
+		if (3 === 4) {
+			console.log("true");
+		} else {
+			console.log("false");
+		}
+		// if (validateInterest.test(interest.val())) {
+		// 	console.log("help!"); 
+		// 	$(this).val(function(index, old) { return old.replace(/[^0-9\.]/g, '') + '.00%'; });
+		// } else {
+  //   		$(this).val(function(index, old) { return old.replace(/[^0-9\.]/g, '') + '%'; });
+  // 		}
 	});
 
 	function calc() {
